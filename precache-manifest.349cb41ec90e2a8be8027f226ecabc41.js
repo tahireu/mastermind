732f0e8d72448c84ba056a5187033a72e66a077f
Damir Tahiri <tahirei@hotmail.com>
2022-05-27 17:14:27 +0200

    Updates

---
+++ precache-manifest.349cb41ec90e2a8be8027f226ecabc41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3c0a46846a1ef48f9a36ca937142a3",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "988edeb57252f52afc72",
+    "url": "/mastermind/static/css/main.5870f614.chunk.css"
+  },
+  {
+    "revision": "1baf8e40b232e1f37726",
+    "url": "/mastermind/static/js/2.1d51a515.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/mastermind/static/js/2.1d51a515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988edeb57252f52afc72",
+    "url": "/mastermind/static/js/main.b851fa92.chunk.js"
+  },
+  {
+    "revision": "93a44bc9e96b6f57e59c",
+    "url": "/mastermind/static/js/runtime-main.7e5e7277.js"
+  },
+  {
+    "revision": "5b18246a1f362def81f970e21585e9fa",
+    "url": "/mastermind/static/media/icomoon.5b18246a.ttf"
+  }
+]);
